bbf06f0f5f8c89aa5ada51b9cf6cfcd94332f189
Mohammad-Vatankhah <mamadvatan80@gmail.com>
2024-10-20 11:40:07 +0330

    navbar refactored

--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -11,13 +11,12 @@ import Link from "next/link";
 
 export default function Navbar() {
   const [user, setUser] = useState<User | undefined>();
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 769);
   const [showModal, setShowModal] = useState(false);
 
   const navLinks = [
     { title: "Home", url: "/" },
+    { title: "Process", url: "/process" },
     { title: "About", url: "/about" },
-    { title: "Services", url: "/services" },
     { title: "Contact", url: "/contact" },
   ];
 
@@ -42,76 +41,61 @@ export default function Navbar() {
     if (accessToken) {
       fetchUser(accessToken);
     }
-
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 769);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
   }, []);
 
   return (
     <>
-      {!isMobile ? (
-        // Laptop Navbar Code Here
-        <nav className="bg-primary text-primary-foreground">
-          <div className="flex justify-between mx-auto items-center py-4 px-24">
-            <div className="text-primary-foreground font-bold text-xl">PM</div>
-            <ul className="flex gap-8 md:gap-16 items-center justify-center text-center cursor-pointer">
-              {navLinks.map((link, index) => (
-                <li key={index} className="text-primary-foreground text-sm">
-                  <Link href={link.url}>{link.title}</Link>
-                </li>
-              ))}
-            </ul>
-            <ul className="flex text-primary-foreground gap-3 items-center cursor-pointer">
-              <UserButtons user={user} setUser={setUser} />
-            </ul>
+      {/* large screen nav */}
+      <nav className="bg-primary text-primary-foreground sticky top-0 z-50 shadow-sm hidden md:block">
+        <div className="flex justify-between mx-auto items-center py-4 px-24">
+          <div className="text-primary-foreground font-bold text-xl">PM</div>
+          <ul className="flex gap-8 md:gap-16 items-center justify-center text-center cursor-pointer">
+            {navLinks.map((link, index) => (
+              <li key={index} className="text-primary-foreground text-md">
+                <Link href={link.url}>{link.title}</Link>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex text-primary-foreground gap-3 items-center cursor-pointer">
+            <UserButtons user={user} setUser={setUser} />
+          </ul>
+        </div>
+      </nav>
+      {/* mobile nav */}
+      <nav className="bg-primary text-primary-foreground py-4 px-4 sticky top-0 z-50 shadow-sm md:hidden">
+        <div className="mx-auto flex justify-between items-center">
+          <div className="text-primary-foreground font-bold text-xl">Logo</div>
+          <div className="flex justify-end items-center gap-3 text-primary-foreground cursor-pointer">
+            <UserButtons user={user} setUser={setUser} />
+            <FaBars
+              onClick={handleBarsIconClick}
+              className="text-primary-foreground cursor-pointer"
+            />
           </div>
-        </nav>
-      ) : (
-        // Mobile Navbar Code Here
-        <nav className="bg-primary text-primary-foreground py-4 px-4">
-          <div className="mx-auto flex justify-between items-center">
-            <div className="text-primary-foreground font-bold text-xl">
-              Logo
-            </div>
-            <div className="flex justify-end items-center gap-3 text-primary-foreground cursor-pointer">
-              <UserButtons user={user} setUser={setUser} />
-              <FaBars
-                onClick={handleBarsIconClick}
-                className="text-primary-foreground cursor-pointer"
-              />
-            </div>
-          </div>
-          {showModal && (
-            <div className="fixed inset-0 flex justify-center items-center">
-              <div className="absolute inset-0 bg-primary" />
-              <FaTimes
-                className="absolute top-6 right-4 text-primary-foreground cursor-pointer"
-                onClick={toggleModal}
-                style={{ fontSize: "16px" }}
-              />
-              <div className="relative bg-primary w-full">
-                <div className="flex flex-col gap-8 items-center justify-center h-full">
-                  {navLinks.map((link, index) => (
-                    <span
-                      key={index}
-                      className="text-secondary-foreground font-light text-2xl cursor-pointer"
-                    >
-                      {link.title}
-                    </span>
-                  ))}
-                </div>
+        </div>
+        {showModal && (
+          <div className="fixed inset-0 flex justify-center items-center">
+            <div className="absolute inset-0 bg-primary" />
+            <FaTimes
+              className="absolute top-6 right-4 text-primary-foreground cursor-pointer"
+              onClick={toggleModal}
+              style={{ fontSize: "16px" }}
+            />
+            <div className="relative bg-primary w-full">
+              <div className="flex flex-col gap-8 items-center justify-center h-full">
+                {navLinks.map((link, index) => (
+                  <span
+                    key={index}
+                    className="text-secondary-foreground font-light text-2xl cursor-pointer"
+                  >
+                    <Link href={link.url}>{link.title}</Link>
+                  </span>
+                ))}
               </div>
             </div>
-          )}
-        </nav>
-      )}
+          </div>
+        )}
+      </nav>
     </>
   );
 }
